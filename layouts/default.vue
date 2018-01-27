da9ef66743ea387c6d6c19e96db089dc3fe0399e
Business Protect <npcprotect@gmail.com>
2018-01-27 18:15:33 +0200

    Minify css

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,7 +21,7 @@
     head: {
       link: [
         { rel:"stylesheet", href:"//fonts.googleapis.com/css?family=Quattrocento|Vollkorn:400,400i,700,700i" },
-        { rel:"stylesheet", href:"/css/main.css" },
+        { rel:"stylesheet", href:"/css/main.min.css" },
         { rel:"apple-touch-icon", href:"/apple-touch-icon.png" },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
       ]
