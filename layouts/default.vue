45b5d0577d5aad6a638f6b55b14b8ec3c75f6bf0
Business Protect <npcprotect@gmail.com>
2018-01-27 18:52:25 +0200

    Add meta tag for google verification

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,6 +19,9 @@
       TheQuote
     },
     head: {
+      meta: [
+        { name:"google-site-verification", content:"A3q8TrFpCQ6wM4oYpIvFrFSffCnhAF1OUjYqsoHdpFw" }
+      ],
       link: [
         { rel:"stylesheet", href:"//fonts.googleapis.com/css?family=Quattrocento|Vollkorn:400,400i,700,700i" },
         { rel:"stylesheet", href:"/css/main.min.css" },
